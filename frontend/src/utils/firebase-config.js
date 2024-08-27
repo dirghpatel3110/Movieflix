1fbf96d207420772e33fa1cb935eff2d67f727fd
dirghpatel3110 <dirghpatel3110@gmail.com>
2024-08-27 09:01:07 -0500

    login and signup

---
+++ frontend/src/utils/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYGJKQgJVnAPFSg7z2UzW_CUouhpUVWlA",
+  authDomain: "react-netflix-clone-c8b64.firebaseapp.com",
+  projectId: "react-netflix-clone-c8b64",
+  storageBucket: "react-netflix-clone-c8b64.appspot.com",
+  messagingSenderId: "115028538086",
+  appId: "1:115028538086:web:a82d25b13c9fd771d8c127",
+  measurementId: "G-SX8QHG04RT"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app);
